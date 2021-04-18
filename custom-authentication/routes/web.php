057a41ca0b06e129e2d43baf55d0778cf4d087ef
Saqlain Mustak <56887504+SaqlainCSE@users.noreply.github.com>
2021-04-18 16:23:31 +0600

    Register

---
+++ custom-authentication/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('register','App\Http\Controllers\Auth\RegisterController@register')->name('register');
+Route::post('register','App\Http\Controllers\Auth\RegisterController@store')->name('register');
